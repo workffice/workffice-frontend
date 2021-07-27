d96271c1b4d96c867e5d74e8ac23c7b827ff915c
Matias E. Navarrete <matias.navarreteutn@gmail.com>
2021-07-26 21:30:58 -0300

    Register submit

--- src/views/pages/Register.js
+++ src/views/pages/Register.js
@@ -43,30 +43,26 @@ function Register() {
     } else if (values.password.length < 8) {
       errors.password = 'Must be 8 characters or more';
     }
-
-    if (!values.userType) {
-      errors.userType = 'Required';
+    if (!values.type) {
+      errors.type = 'Required';
     }
-
     if (!values.email) {
       errors.email = 'Required';
     } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
       errors.email = 'Invalid email address';
     }
-
     return errors;
   };
 
-
   const formik = useFormik({
     initialValues: {
-      email: '',
-      password: '',
-      userType: '',
+      email: 'admin@mail.com',
+      password: '123456789',
+      type: 'OFFICE_HOLDER',
     },
     validate,
     onSubmit: values => {
-      alert(JSON.stringify(values, null, 2));
+      alert(JSON.stringify(values, null, 3));
     },
   });
 
@@ -84,13 +80,13 @@ function Register() {
             <RegisterInfo />
           </Col>
           <Col className="mr-auto" lg="4" md="6">
-            <Card className="card-signup text-center">
-              <CardHeader>
-                <CardTitle tag="h4">Creemos tu cuenta</CardTitle>
-              </CardHeader>
-              <CardBody>
-                {/* FORMULARIO REGISTRO*/}
-                <Form action="" className="form" method="" onSubmit={formik.handleSubmit}>
+            {/* FORMULARIO REGISTRO*/}
+            <Form className="form" onSubmit={formik.handleSubmit}>
+              <Card className="card-signup text-center">
+                <CardHeader>
+                  <CardTitle tag="h4">Creemos tu cuenta</CardTitle>
+                </CardHeader>
+                <CardBody>
                   <FormGroup className={formik.errors.email && formik.touched.email ? 'has-danger' : 'has-success'}>
                     <InputGroup>
                       <InputGroupAddon addonType="prepend">
@@ -106,7 +102,7 @@ function Register() {
                         onBlur={formik.handleBlur}
                         value={formik.values.email} />
                     </InputGroup>
-                    {formik.errors.email && formik.touched.email && formik.validateOnChange.email? <div className="error">{formik.errors.email}</div> : null}
+                    {formik.errors.email && formik.touched.email && formik.validateOnChange.email ? <div className="error">{formik.errors.email}</div> : null}
                   </FormGroup>
                   <FormGroup className={formik.errors.password && formik.touched.password ? 'has-danger' : 'has-success'}>
                     <InputGroup>
@@ -124,26 +120,28 @@ function Register() {
                         value={formik.values.password}
                       />
                     </InputGroup>
-                    {formik.errors.password && formik.touched.password  && formik.validateOnChange.password? <div className="error">{formik.errors.password}</div> : null}
+                    {formik.errors.password && formik.touched.password && formik.validateOnChange.password ? <div className="error">{formik.errors.password}</div> : null}
                   </FormGroup>
                   <FormGroup>
-                    <Input type="select" name="userType" id="select">
+                    <Input type="select" name="type" id="select" onChange={formik.handleChange} value={formik.values.type}>
                       <option value="RENTER">Usuario Cliente</option>
                       <option value="OFFICE_HOLDER">Propietario de oficinas</option>
                     </Input>
                   </FormGroup>
-                </Form>
-              </CardBody>
-              <CardFooter>
-                <Button
-                  className="btn-round"
-                  color="info"
-                  type="submit"
-                >
-                  Crear cuenta
-                </Button>
-              </CardFooter>
-            </Card>
+                </CardBody>
+                <CardFooter>
+                  <Button
+                    className="btn-round"
+                    color="info"
+                    type="submit"
+                    disabled={formik.isSubmitting}
+                  >
+                    Crear cuenta
+                  </Button>
+                </CardFooter>
+
+              </Card>
+            </Form>
           </Col>
         </Row>
       </Container>
