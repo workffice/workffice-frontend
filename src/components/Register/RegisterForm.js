8bfd9f383d58f0b5b2132ae1e74ce29554007971
Matias E. Navarrete <matias.navarreteutn@gmail.com>
2021-08-11 18:12:49 -0300

    fix git ignore

---
+++ src/components/Register/RegisterForm.js
@@ -0,0 +1,113 @@
+import React from 'react'
+import { useFormik } from "formik";
+import {
+    Button,
+    Card,
+    CardHeader,
+    CardBody,
+    CardFooter,
+    CardTitle,
+    FormGroup,
+    Form,
+    Input,
+    InputGroupAddon,
+    InputGroupText,
+    InputGroup
+} from "reactstrap";
+
+
+
+export const RegisterForm = () => {
+
+    const validate = values => {
+        const errors = {};
+        if (!values.password) {
+            errors.password = 'Required';
+        } else if (values.password.length < 8) {
+            errors.password = 'Must be 8 characters or more';
+        }
+        if (!values.type) {
+            errors.type = 'Required';
+        }
+        if (!values.email) {
+            errors.email = 'Required';
+        } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
+            errors.email = 'Invalid email address';
+        }
+        return errors;
+    };
+    
+    const formik = useFormik({
+        initialValues: {
+            email: 'admin@mail.com',
+            password: '123456789',
+            type: 'OFFICE_HOLDER',
+        },
+        validate,
+        onSubmit: values => {
+            alert(JSON.stringify(values, null, 3));
+        },
+    });
+    return (
+        <Form className="form" onSubmit={formik.handleSubmit}>
+            <Card className="card-signup text-center">
+                <CardHeader>
+                    <CardTitle tag="h4">Creemos tu cuenta</CardTitle>
+                </CardHeader>
+                <CardBody>
+                    <FormGroup className={formik.errors.email && formik.touched.email ? 'has-danger' : 'has-success'}>
+                        <InputGroup>
+                            <InputGroupAddon addonType="prepend">
+                                <InputGroupText>
+                                    <i className="fa fa-user" />
+                                </InputGroupText>
+                            </InputGroupAddon>
+                            <Input
+                                name="email"
+                                placeholder="Email.."
+                                type="email"
+                                onChange={formik.handleChange}
+                                onBlur={formik.handleBlur}
+                                value={formik.values.email} />
+                        </InputGroup>
+                        {formik.errors.email && formik.touched.email && formik.validateOnChange.email ? <div className="error">{formik.errors.email}</div> : null}
+                    </FormGroup>
+                    <FormGroup className={formik.errors.password && formik.touched.password ? 'has-danger' : 'has-success'}>
+                        <InputGroup>
+                            <InputGroupAddon addonType="prepend">
+                                <InputGroupText>
+                                    <i className="fa fa-key" />
+                                </InputGroupText>
+                            </InputGroupAddon>
+                            <Input
+                                name="password"
+                                placeholder="Contraseña..."
+                                type="password"
+                                onChange={formik.handleChange}
+                                onBlur={formik.handleBlur}
+                                value={formik.values.password}
+                            />
+                        </InputGroup>
+                        {formik.errors.password && formik.touched.password && formik.validateOnChange.password ? <div className="error">{formik.errors.password}</div> : null}
+                    </FormGroup>
+                    <FormGroup>
+                        <Input type="select" name="type" id="select" onChange={formik.handleChange} value={formik.values.type}>
+                            <option value="RENTER">Usuario Cliente</option>
+                            <option value="OFFICE_HOLDER">Propietario de oficinas</option>
+                        </Input>
+                    </FormGroup>
+                </CardBody>
+                <CardFooter>
+                    <Button
+                        className="btn-round"
+                        color="info"
+                        type="submit"
+                        disabled={formik.isSubmitting}
+                    >
+                        Crear cuenta
+                    </Button>
+                </CardFooter>
+            </Card>
+        </Form>
+    )
+}
