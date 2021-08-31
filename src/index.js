d78772ee65ff4a7b12ecbc898b5c45928a360c3b
Teresita <terecuccia@gmail.com>
2021-08-30 22:58:49 -0300

    routes of colaborator page removed

--- src/index.js
+++ src/index.js
@@ -8,23 +8,12 @@ import { Routes } from './infra/init/routes';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'perfect-scrollbar/css/perfect-scrollbar.css';
 import './assets/scss/paper-dashboard.scss?v=1.3.0';
-import { ColaboratorScreen } from './components/colaborator/ColaboratorScreen';
-import { NewColaborator } from './components/colaborator/NewColaborator';
 
 const Init = () => {
   const history = getHistory();
 
   return (
     <Provider store={buildStore(reducers, history)}>
-      <BrowserRouter>
-        <Switch>
-          <Route path="/auth" render={props => <AuthLayout {...props} />} />
-          <Route path="/admin" render={props => <AdminLayout {...props} />} />
-          <Route path="/admin/colaborators" render={() => <ColaboratorScreen/>} />
-          <Route path="/admin/newColaborator" render={() => <NewColaborator/>} />
-          <Redirect to="/auth/login" />
-        </Switch>
-      </BrowserRouter>
       <Routes history={history} />
     </Provider>
   );
