a932d66ee36be1e38fa1ab729a2f411ebe01008e
nicolas.nunez <nicolas.nunez@eventbrite.com>
2021-10-12 01:29:14 -0300

    Add notification in user profile update

--- src/api/backoffice/users.js
+++ src/api/backoffice/users.js
@@ -1,7 +1,11 @@
-import { getMe } from "../../infra/api/backoffice/users";
+import { getMe, updateUser } from "../../infra/api/backoffice/users";
 
 
 export const userMeAPI = async () => {
     const userMe = await getMe();
     return userMe;
+}
+
+export const updateUserApi = async (userId, userBody) => {
+    return Promise.resolve(updateUser(userId, userBody))
 }
