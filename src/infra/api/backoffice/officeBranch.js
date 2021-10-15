326831944d519f62df1208df5f56a63a54633158
nicolas.nunez <nicolas.nunez@eventbrite.com>
2021-10-14 22:51:08 -0300

    Add cloudinary to office branch update form

--- src/infra/api/backoffice/officeBranch.js
+++ src/infra/api/backoffice/officeBranch.js
@@ -2,6 +2,17 @@ import { headerGet, headersPost, sdkAuthRequest, sdkNoAuthRequest } from "..";
 import { API_OFFICE_BRANCHES, API_OFFICE_HOLDERS } from "../../../environments/environment";
 import { writeToLocalStorage } from "../localStorage";
 
+const postImageToCloudinary = async imageData => {
+    const data = new FormData()
+    data.append("file", imageData)
+    data.append("upload_preset", "testworkffice")
+    data.append("cloud_name", "workffice")
+    return await fetch("https://api.cloudinary.com/v1_1/workffice/image/upload", {
+        method: "post",
+        body: data
+    }).then(resp => resp.json())
+}
+
 export const createOfficeBranchInfra = async ({
     name,
     description,
@@ -13,14 +24,7 @@ export const createOfficeBranchInfra = async ({
     zipCode
 }, userId) => {
     try {
-        const data = new FormData()
-        data.append("file", image)
-        data.append("upload_preset", "testworkffice")
-        data.append("cloud_name", "workffice")
-        const imageData = await fetch("  https://api.cloudinary.com/v1_1/workffice/image/upload", {
-            method: "post",
-            body: data
-        }).then(resp => resp.json())
+        const imageData = await postImageToCloudinary(image)
         const officeBranch = await sdkAuthRequest(
             `${API_OFFICE_HOLDERS}/${userId}/office_branches/`,
             {
@@ -45,14 +49,33 @@ export const createOfficeBranchInfra = async ({
 
 }
 
-export const editOfficeBranchInfra = async (officeBranchData, officeBranchId) => {
+export const editOfficeBranchInfra = async ({
+    name,
+    description,
+    phone,
+    image,
+    province,
+    city,
+    street,
+    zipCode
+}, officeBranchId) => {
     try {
+        const imageData = await postImageToCloudinary(image)
         const officeBranch = await sdkAuthRequest(
             `${API_OFFICE_BRANCHES}/${officeBranchId}/`,
             {
                 method: 'PUT',
                 headers: headersPost,
-                body: JSON.stringify(officeBranchData)
+                body: JSON.stringify({
+                    name,
+                    description,
+                    phone,
+                    province,
+                    city,
+                    street,
+                    zipCode,
+                    imagesUrls: [imageData.public_id],
+                })
             }
         );
         return Promise.resolve(officeBranch.data);
