d6450f8af1529eba232ef592c8377edef197a34c
nicolas.nunez <nicolas.nunez@eventbrite.com>
2021-10-18 18:49:26 -0300

    Move notification reducer to notification directory

--- src/stores/index.js
+++ src/stores/index.js
@@ -1,7 +1,6 @@
 import { connectRouter } from 'connected-react-router';
 import { combineReducers } from 'redux';
 import { LOADING } from './actions';
-import { HIDE_NOTIFICATION, SET_ERROR, SET_SUCCESS } from './actions/notifications/writeNotificationActions';
 import { activateAccountReducer } from './reducers/auth/activateAccountReducer';
 import { activatePasswordReducer } from './reducers/auth/activatePasswordReducer';
 import { loginReducer } from './reducers/auth/loginReducer';
@@ -20,6 +19,7 @@ import { collaboratorRolesReducer, rolesReducer } from './reducers/backoffice/ro
 import { userMeReducer } from './reducers/backoffice/userReducer';
 import { entityNotFoundReducer } from './reducers/errors/notFoundReducer';
 import { permissionReducer } from './reducers/errors/permissionReducer';
+import { notificationReducer } from './reducers/notification/writeNotificationReducer';
 
 const isLoadingReducer = (state = false, { type, payload }) => {
   let currentState = state;
@@ -27,45 +27,6 @@ const isLoadingReducer = (state = false, { type, payload }) => {
     currentState = payload;
   }
   return currentState;
-};
-
-
-const notificationInitialState = {
-  message: null,
-  isSuccess: false,
-  isError: false,
-  show: false,
-  errorCode: null,
-}
-const notificationReducer = (state = notificationInitialState, { type, payload }) => {
-  switch (type) {
-    case SET_SUCCESS:
-      return {
-        message: payload.message ? payload.message : null,
-        errorCode: null,
-        isSuccess: true,
-        isError: false,
-        show: true,
-      }
-    case SET_ERROR:
-      return {
-        message: payload.message ? payload.message : null,
-        errorCode: payload.error ? payload.error : null,
-        isSuccess: false,
-        isError: true,
-        show: true,
-      }
-    case HIDE_NOTIFICATION:
-      return {
-        message: null,
-        errorCode: null,
-        isSuccess: false,
-        isError: false,
-        show: false
-      }
-    default:
-      return state
-  }
 };
 
 
