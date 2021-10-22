ab92f6a6553e20bed3d03ee0729b30edc8fd5c21
nicolas.nunez <nicolas.nunez@eventbrite.com>
2021-10-21 22:37:26 -0300

    Add mercado pago checkout pro

--- src/infra/api/backoffice/booking.js
+++ src/infra/api/backoffice/booking.js
@@ -1,15 +1,27 @@
-import { headersPost, sdkAuthRequest } from "..";
+import { headerGet, headersPost, sdkAuthRequest } from "..";
 import { API_URL } from "../../../environments/environment";
 
 export const bookOffice = async (officeId, bookingBody) => {
-  try {
-    const booking = await sdkAuthRequest(`${API_URL}/offices/${officeId}/bookings/`, {
-      method: 'POST',
-      headers: headersPost,
-      body: JSON.stringify(bookingBody)
-    });
-    return Promise.resolve(booking.data.uri.match("/api/bookings/(.*)/")[1]);
-  } catch (error) {
-    return Promise.reject(error.errors[0]);
-  }
+    try {
+        const booking = await sdkAuthRequest(`${API_URL}/offices/${officeId}/bookings/`, {
+            method: 'POST',
+            headers: headersPost,
+            body: JSON.stringify(bookingBody)
+        });
+        return Promise.resolve(booking.data.uri.match("/api/bookings/(.*)/")[1]);
+    } catch (error) {
+        return Promise.reject(error.errors[0]);
+    }
 };
+
+export const getBooking = async bookingId => {
+    try {
+        const booking = await sdkAuthRequest(`${API_URL}/bookings/${bookingId}/`, {
+            method: 'GET',
+            headers: headerGet,
+        });
+        return Promise.resolve(booking.data);
+    } catch (error) {
+        return Promise.reject(error.errors[0]);
+    }
+}
