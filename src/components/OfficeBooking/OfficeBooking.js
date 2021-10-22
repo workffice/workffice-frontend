cc265eb872dc2320ca6adc2d2ded8a5aeef2686a
nicolas.nunez <nicolas.nunez@eventbrite.com>
2021-10-21 21:03:52 -0300

    Add form validation

--- src/components/OfficeBooking/OfficeBooking.js
+++ src/components/OfficeBooking/OfficeBooking.js
@@ -1,4 +1,5 @@
 import { useFormik } from 'formik';
+import moment from 'moment';
 import React, { useEffect } from 'react';
 import Datetime from 'react-datetime';
 import { useLocation } from 'react-router';
@@ -22,27 +23,25 @@ export const OfficeBooking = ({ office, loadOffice, notification, hideNotificati
 
     const validate = values => {
         const errors = {};
-        if (!values.date) {
+        if (!values.date)
             errors.date = 'Requerido.';
-        }
-        if (!values.startTime) {
+        if (!values.startTime)
             errors.startTime = 'Requerido.';
-        }
-        if (!values.endTime) {
+        if (!values.endTime)
             errors.endTime = 'Requerido.';
-        }
-        if (!values.numberOfAssistants) {
+        if (!values.numberOfAssistants)
             errors.numberOfAssistants = 'Requerido.';
-        }
+        if (values.endTime._d.getHours() - values.startTime._d.getHours() <= 0)
+            errors.invalidTimeRange = 'La hora de ingreso debe ser previa a la hora de salida'
         return errors;
     };
 
 
     const formik = useFormik({
         initialValues: {
-            date: null,
-            startTime: null,
-            endTime: null,
+            date: moment(),
+            startTime: moment().hour(8),
+            endTime: moment().hour(9),
             numberOfAssistants: 0,
         },
         validate,
@@ -55,10 +54,6 @@ export const OfficeBooking = ({ office, loadOffice, notification, hideNotificati
             console.log(booking)
         },
     });
-
-    const price = 500;
-    const hour = 5;
-
 
     return (
         <div className="content">
@@ -89,40 +84,56 @@ export const OfficeBooking = ({ office, loadOffice, notification, hideNotificati
                                 <Label htmlFor="date" className="label-form">Fecha</Label>
                                 <FormGroup>
                                     <Datetime
+                                        initialValue={formik.values.date}
                                         name="date"
                                         id="date"
                                         onChange={value => formik.setFieldValue("date", value)}
                                         timeFormat={false}
                                         inputProps={{ placeholder: "Seleccione una fecha" }}
+                                        isValidDate={current => {
+                                            const yesterday = moment().subtract(1, 'day');
+                                            return current.isAfter(yesterday);
+                                        }}
                                     />
                                 </FormGroup>
                             </FormGroup>
 
-                            <FormGroup className={formik.errors.date ? 'has-danger' : ''}>
-                                <Label htmlFor="startTime" lassName="label-form">Hora desde</Label>
-                                <FormGroup>
-                                    <Datetime
-                                        name="startTime"
-                                        id="startTime"
-                                        onChange={value => formik.setFieldValue("startTime", value)}
-                                        dateFormat={false}
-                                        inputProps={{ placeholder: "Seleccione la hora de ingreso" }}
-                                    />
-                                </FormGroup>
-                            </FormGroup>
+                            <Row>
+                                <Col md="6">
+                                    <Label htmlFor="startTime" className="label-form">Hora ingreso</Label>
+                                    <FormGroup className={formik.errors.startTime ? 'has-danger' : ''}>
+                                        <Datetime
+                                            name="startTime"
+                                            id="startTime"
+                                            initialValue={formik.values.startTime}
+                                            onChange={value => formik.setFieldValue("startTime", value)}
+                                            dateFormat={false}
+                                            timeFormat="HH"
+                                            input={false}
+                                            timeConstraints={{ hours: { min: 8, max: 17 } }}
+                                            inputProps={{ placeholder: "Seleccione la hora de ingreso" }}
+                                        />
+                                    </FormGroup>
+                                </Col>
+                                <Col md="6">
+                                    <FormGroup className={formik.errors.endTime ? 'has-danger' : ''}>
+                                        <Label htmlFor="endTime" className="label-form">Hora salida</Label>
+                                        <Datetime
+                                            name="endTime"
+                                            id="endTime"
+                                            initialValue={formik.values.endTime}
+                                            onChange={value => formik.setFieldValue("endTime", value)}
+                                            dateFormat={false}
+                                            timeFormat="HH"
+                                            input={false}
+                                            timeConstraints={{ hours: { min: 9, max: 18 } }}
+                                            inputProps={{ placeholder: "Seleccione la hora de salida" }}
+                                        />
+                                    </FormGroup>
+                                </Col>
+                            </Row>
+                            {formik.errors.invalidTimeRange ? <Label className="text-danger">{formik.errors.invalidTimeRange}</Label> : <></>}
 
-                            <FormGroup className={formik.errors.date ? 'has-danger' : ''}>
-                                <Label htmlFor="endTime" className="label-form">Hora hasta</Label>
-                                <FormGroup>
-                                    <Datetime
-                                        name="endTime"
-                                        id="endTime"
-                                        onChange={value => formik.setFieldValue("endTime", value)}
-                                        dateFormat={false}
-                                        inputProps={{ placeholder: "Seleccione la hora de egreso" }}
-                                    />
-                                </FormGroup>
-                            </FormGroup>
 
                             <FormGroup className={formik.errors.numberOfAssistants ? 'has-danger' : ''}>
                                 <Label htmlFor="numberOfAssistants" className="label-form">Cantidad de personas</Label>
@@ -138,7 +149,16 @@ export const OfficeBooking = ({ office, loadOffice, notification, hideNotificati
                             </FormGroup>
 
                             <FormGroup>
-                                <Label className="label-form">{` Precio total $ ${price * hour}`} </Label>
+                                {
+                                    office
+                                        ? <Label className="label-form">
+                                            {` Precio total $ ${office.price * (formik.values.endTime._d.getHours() - formik.values.startTime._d.getHours())}`}
+                                        </Label>
+                                        : <Label className="label-form">
+                                            Precio total $
+                                        </Label>
+                                }
+
                             </FormGroup>
                             <Row>
                                 <Col style={{ display: 'flex', justifyContent: 'center' }}>
@@ -169,7 +189,7 @@ export const OfficeBooking = ({ office, loadOffice, notification, hideNotificati
                             </CardTitle>
                         </CardHeader>
                         <CardFooter>
-                            {inactivities && inactivities.map(inactivity => <Badge color="info">{inactivity.dayOfWeek}</Badge>)}
+                            {inactivities && inactivities.map(inactivity => <Badge key={inactivity.id} color="info">{inactivity.dayOfWeek}</Badge>)}
                         </CardFooter>
                     </Col>
                 </Row>
