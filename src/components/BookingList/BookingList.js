227c9e79200bcdecd7acfa3a020524a924bf71c9
niconunez96 <nicolas110996@gmail.com>
2021-10-26 11:40:16 -0300

    Comment date selector

--- src/components/BookingList/BookingList.js
+++ src/components/BookingList/BookingList.js
@@ -1,31 +1,34 @@
-import { useFormik } from 'formik';
-import moment from 'moment';
+// import { useFormik } from 'formik';
+// import moment from 'moment';
 import React, { useEffect } from 'react';
-import ReactDatetimeClass from 'react-datetime';
+// import ReactDatetimeClass from 'react-datetime';
+// import {
+//     Col, Form,
+//     FormGroup, Label, Row
+// } from 'reactstrap';
 import {
-    Col, Form,
-    FormGroup, Label, Row
+    Col, Row
 } from 'reactstrap';
 import { BookingListComponent } from './BookingListComponent';
 
 export const BookingList = ({ user, bookings, loadBookings }) => {
 
-    const validate = values => {
-        const errors = {};
-        if (!values.date)
-            errors.date = 'Requerido.';
-        return errors;
-    };
+    // const validate = values => {
+    //     const errors = {};
+    //     if (!values.date)
+    //         errors.date = 'Requerido.';
+    //     return errors;
+    // };
 
-    const formik = useFormik({
-        initialValues: {
-            date: moment(),
-        },
-        validate,
-        onSubmit: async ({ date }) => {
-            console.log(date);
-        }
-    })
+    // const formik = useFormik({
+    //     initialValues: {
+    //         date: moment(),
+    //     },
+    //     validate,
+    //     onSubmit: async ({ date }) => {
+    //         console.log(date);
+    //     }
+    // })
     useEffect(() => {
         if (user)
             loadBookings(user.email)
@@ -46,7 +49,7 @@ export const BookingList = ({ user, bookings, loadBookings }) => {
                     <hr />
                 </Col>
             </Row>
-            <Form onSubmit={formik.handleSubmit}>
+            {/* <Form onSubmit={formik.handleSubmit}>
                 <Row style={{ display: "flex", justifyContent: "center", marginBottom: 20 }}>
                     <Label htmlFor="date" className="label-form" style={{ fontSize: 20 }}>Seleccione una fecha</Label>
                     <FormGroup style={{ marginLeft: 20, marginTop: 10 }}>
@@ -64,7 +67,7 @@ export const BookingList = ({ user, bookings, loadBookings }) => {
                         />
                     </FormGroup>
                 </Row>
-            </Form>
+            </Form> */}
             {
                 bookings.map(booking => {
                     return <BookingListComponent
@@ -76,8 +79,8 @@ export const BookingList = ({ user, bookings, loadBookings }) => {
                         scheduleDate={formatDate(booking.startTime)}
                         startTime={formatHour(booking.startTime)}
                         endTime={formatHour(booking.endTime)}
-                        transactionAmount={booking.paymentInformation ? booking.paymentInformation.transactionAmount : "No definido"}
-                        providerFee={booking.paymentInformation ? booking.paymentInformation.providerFee : "No definido"}
+                        transactionAmount={booking.totalAmount}
+                        providerFee={booking.paymentInformation ? booking.paymentInformation.providerFee : "-"}
                         currency={booking.paymentInformation ? booking.paymentInformation.currency : "No definido"}
                         paymentMethodId={booking.paymentInformation ? booking.paymentInformation.paymentMethodId : "No definido"}
                         paymentTypeId={booking.paymentInformation ? booking.paymentInformation.paymentTypeId : "No definido"}
@@ -85,51 +88,6 @@ export const BookingList = ({ user, bookings, loadBookings }) => {
                     />
                 })
             }
-            <BookingListComponent
-                officeName="Oficina número 1"
-                id="0cfad0be-69e9-469a-85e5-803507516fc7"
-                status="SCHEDULED"
-                attendeesQuantity={5}
-                created="05/10/1996"
-                startTime="05:30"
-                endTime="14:20"
-                transactionAmount={1000}
-                providerFee={200}
-                currency="ARS"
-                paymentMethodId="master"
-                paymentTypeId="tarjeta de crédito"
-                officeBranchName="Whale"
-            />
-            <BookingListComponent
-                officeName="Oficina número 1"
-                id="0cfad0be-69e9-469a-85e5-803507516fc7"
-                status="PENDING"
-                attendeesQuantity={5}
-                created="05/10/1996"
-                startTime="05:30"
-                endTime="14:20"
-                transactionAmount={1000}
-                providerFee={200}
-                currency="ARS"
-                paymentMethodId="master"
-                paymentTypeId="tarjeta de crédito"
-                officeBranchName="Whale"
-            />
-            <BookingListComponent
-                officeName="Oficina número 1"
-                id="0cfad0be-69e9-469a-85e5-803507516fc7"
-                status="CANCELLED"
-                attendeesQuantity={5}
-                created="05/10/1996"
-                startTime="05:30"
-                endTime="14:20"
-                transactionAmount={1000}
-                providerFee={200}
-                currency="ARS"
-                paymentMethodId="master"
-                paymentTypeId="tarjeta de crédito"
-                officeBranchName="Whale"
-            />
         </div >
     )
 }
