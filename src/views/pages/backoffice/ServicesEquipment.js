6e61f5927407ff0dc2b559cd3910f785e5a93f0a
nicolas.nunez <nicolas.nunez@eventbrite.com>
2021-12-07 03:15:25 -0300

    Fix services category

--- src/views/pages/backoffice/ServicesEquipment.js
+++ src/views/pages/backoffice/ServicesEquipment.js
@@ -11,7 +11,7 @@ export const ServicesEquipment = (props) => {
         },
         { value: "FOOD", label: "Comida" },
         { value: "CONFORT", label: "Confort" },
-        { value: "CONECTIVITY", label: "Conectividad" },
+        { value: "CONNECTIVITY", label: "Conectividad" },
     ];
 
     const equipaments = [
